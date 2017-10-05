5fdd258fb9848682ff75bbea861c2be3351e1b0a
luandryl <luan.andryl@gmail.com>
2017-10-05 04:39:16 -0300

    Reset Password Implemented

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -2,38 +2,80 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Hash;
+use JWTAuth;
+use App\User;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+
 
 class ResetPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reset Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller is responsible for handling password reset requests
+	| and uses a simple trait to include this behavior. You're free to
+	| explore this trait and override any methods you wish to tweak.
+	|
+	*/
 
-    use ResetsPasswords;
+	public function resetPassword(Request $req) {
 
-    /**
-     * Where to redirect users after resetting their password.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+		if (!$this->validator($req->all())->fails()) {
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
+			$data = $req->all();
+
+			$user = User::where('email', '=', $req->get('email'))->first();
+
+			$user->password = bcrypt($data['new_password']);
+		
+			if (!$user->save()) {
+				return response()->json([
+					'error' => 'Could not save the User'
+				], 500);
+			}
+			
+			$token = JWTAuth::getToken();
+			
+			$new_token = JWTAuth::refresh($token);
+			$objectToken = JWTAuth::setToken($new_token);
+			$expiration = JWTAuth::decode($objectToken->getToken())->get('exp');
+	
+			return response()->json([
+				'access_token' => $new_token,
+				'token_type' => 'bearer',
+				'expires_in' => $expiration
+			]);
+
+		} 
+
+		return response()->json([
+			'message'   => 'Validation Failed',
+			'errors'    => $this->validator($req->all())->errors()->all()
+		], 422);
+	
+			
+	}
+
+	/**
+		* Get a validator for an incoming registration request.
+		*
+		* @param  array  $data
+		* @return \Illuminate\Contracts\Validation\Validator
+		*/
+		protected function validator(array $data) {
+			return Validator::make($data, [
+				'email' => 'required|string|email|max:255',
+				'password' => 'required|string|min:2',
+				'new_password' => 'required|string|min:6|different:password',
+				'new_password_confirmation' => 'required|string|min:6|same:new_password'
+			]);
+		}
+
 }
